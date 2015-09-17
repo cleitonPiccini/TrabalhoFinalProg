0180d6d0a2a6877134d86b0129386b8ccc534377
cleitonPiccini <cleiton-piccini@hotmail.com>
2015-09-17 19:10:09 -0300

    Classe date

---
+++ Exercicio1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Exercicio1 {
+
+	public static void main (Sting [] args){
+
+		Date d = new Date();
+
+		if (d.getDay == 0){
+
+			System.out.prinln ("Promoção 1");
+		}
+
+		if (d.getDay == 1){
+
+			System.out.prinln ("Promoção 2");
+		}
+
+		if (d.getDay == 2){
+
+			System.out.prinln ("Promoção 3");
+		}
+
+		if (d.getDay == 3){
+
+			System.out.prinln ("Promoção 4");
+		}
+
+		if (d.getDay == 4){
+
+			System.out.prinln ("Promoção 5");
+		}
+
+		if (d.getDay == 5){
+
+			System.out.prinln ("Promoção 6");
+		}
+
+		if (d.getDay == 6){
+
+			System.out.prinln ("Promoção 7");
+		}
+	}
+	
+
+
+}
